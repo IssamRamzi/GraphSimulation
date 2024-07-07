d7dc4121cc32ea6a5fcdbccd883b72d031601765
AZIZImohammed <artazizi06@gmail.com>
2024-07-07 13:35:47 +0100

    Fixing Errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "raylib.h"
+#include "../cmake-build-debug/_deps/raylib-src/src/raylib.h"
 #include "headers/Node.h"
 #include "pages/Home.h"
 #include "pages/Page.h"
